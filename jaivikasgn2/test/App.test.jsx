f65f4786215ca79e0fa27cead2d67a9cf31f673d
jaivik1920 <pateljaivik2025@gmail.com>
2024-10-08 22:01:27 -0400

    Adding test cases

--- jaivikasgn2/test/App.test.jsx
+++ jaivikasgn2/test/App.test.jsx
@@ -1,11 +1,11 @@
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import App from '../src/App';
 
 test('renders Hello DevOps text', () => {
   render(<App/>)
-
   const helloElement = screen.getByText(/Hello DevOps!/i);
 
-  //checking that hello devops present in document or not.
+  // checking that hello devops present in document or not.
   expect(helloElement).toBeInTheDocument();
 });
